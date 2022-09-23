ac0b042717102f1e380cd0c27dfc63f45c9a640a
Matthew Abraham <wasteofintel@gmail.com>
2022-09-23 10:47:22 -0400

    feat: invalidate access token

--- src/auth/actions.ts
+++ src/auth/actions.ts
@@ -2,6 +2,7 @@ import {Auth0AuthorizationResponse} from './types';
 
 export enum Actions {
   SET_ACCESS_TOKEN = 'AUTH/SET_ACCESS_TOKEN',
+  INVALIDATE_ACCESS_TOKEN = 'AUTH/INVALIDATE_ACCESS_TOKEN',
 }
 
 export interface SetAccessToken {
@@ -9,11 +10,19 @@ export interface SetAccessToken {
   authorizationToken: Auth0AuthorizationResponse;
 }
 
+export interface InvalidateAccessToken {
+  type: Actions.INVALIDATE_ACCESS_TOKEN;
+}
+
 export const setAccessToken = (
   authorizationToken: Auth0AuthorizationResponse,
 ): SetAccessToken => ({
   type: Actions.SET_ACCESS_TOKEN,
   authorizationToken,
+});
+
+export const invalidateAccessToken = (): InvalidateAccessToken => ({
+  type: Actions.INVALIDATE_ACCESS_TOKEN,
 });
 
 export type ActionTypes = SetAccessToken;
